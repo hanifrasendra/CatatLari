47c8c000fd230f6ebbaf8ebccdf54b9b0df80b3a
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2026-03-05 19:32:14 +0700

    fix merge

--- app/src/main/java/com/example/catatanlari/MainActivity.kt
+++ app/src/main/java/com/example/catatanlari/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.fragment_registrasi)
+        setContentView(R.layout.fragment_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registrasi)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
